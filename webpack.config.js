5f6cb139fa6d0c8c8ebe53f2cbbc075c59d7adbc
Fazrad Karkhani <farzad.karkhani@gmail.com>
2018-01-29 13:27:21 +0330

    first config for start

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+var ZipPlugin = require('zip-webpack-plugin');
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const parts = require('./webpack.parts');
+const PATHS = {
+  index: path.join(__dirname, 'src'),
+  dist: path.join(__dirname, 'dist'),
+};
+
+
+const commonConfig = merge([
+  {
+    output: {
+      path: PATHS.dist,
+      filename: '[name].js',
+      publicPath: '/'
+    },
+  },
+  parts.loadFonts({
+    options: {
+      name: "[name].[ext]",
+    },
+  }),
+  parts.loadJavaScript({ include: PATHS.dist }),
+]);
+
+const productionConfig = merge([
+  {
+    plugins: [
+      new BundleAnalyzerPlugin({
+          analyzerMode: 'static'
+      }),
+      new ZipPlugin({
+        filename: 'limo-' + require('./package.json').version + '.zip',
+      }),
+    ],
+  },
+  parts.clean(PATHS.dist),
+  parts.minifyJavaScript(),
+  parts.minifyCSS({
+    options: {
+      discardComments: {
+        removeAll: true,
+      },
+      safe: true,
+    },
+  }),
+  parts.generateSourceMaps({ type: 'source-map' }),
+  parts.extractCSS({
+    use: ['css-loader', parts.autoprefix()],
+  }),
+  parts.attachCopyrightBanner(),
+  parts.setFreeVariable(
+    'process.env.NODE_ENV',
+    'production'
+  ),
+]);
+
+const developmentConfig = merge([
+  {
+    output: {
+      devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]',
+    },
+  },
+  parts.generateSourceMaps({ type: 'cheap-module-eval-source-map' }),
+  parts.loadCSS(),
+  parts.devServer({
+    host: 'farzad.local',
+    port: 3333,
+  }),
+  parts.setFreeVariable(
+    'process.env.NODE_ENV',
+    'development'
+  ),
+]);
+
+module.exports = (env) => {
+  const pages = [
+    parts.page({
+      title: 'Limo',
+      entry: {
+        limo: PATHS.index,
+      },
+    }),
+  ];
+  const config = env === 'production' ?
+    productionConfig :
+    developmentConfig;
+
+  return merge([commonConfig, config].concat(pages));
+};
